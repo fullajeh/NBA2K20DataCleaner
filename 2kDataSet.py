f9b66760804bfe0a06f0397d1faef384dc3bbb7a
fullajeh <fullajeh@gmail.com>
2025-08-06 11:25:30 -0500

    Updated with Comments - 2kDataSet.py
    Added comments to narrate my thought process as I was doing this project.

--- 2kDataSet.py
+++ 2kDataSet.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 from scipy import stats
 
 csv = pd.read_csv("nba2k-full.csv")
-csv.dropna()
-csv.drop_duplicates()
-csv.columns = [col.strip().lower().replace(" ", "_") for col in csv.columns]
+csv.dropna() #Drop null values
+csv.drop_duplicates() #Drop duplicate values
+csv.columns = [col.strip().lower().replace(" ", "_") for col in csv.columns] #Seperate the Comma and Space seperated values into columns.
 
 
 print(csv.head)
@@ -14,13 +14,9 @@ print(csv.info)
 ratings = csv.sort_values(by="rating", ascending=False).head(15)
 print(ratings)
 
-sample_dataGuards = csv[]
-
-plt.bar(ratings["full_name"], ratings['rating'])
-plt.xticks(rotation=45)
-plt.xlabel("Players")
-plt.ylabel("Overall Ratings")
-plt.tight_layout()
+plt.bar(ratings["full_name"], ratings['rating']) #Set the X and Y axis data for a Bar Chart.
+plt.xticks(rotation=45) # Rotate the player's names so they won't get cut off.
+plt.xlabel("Players") # X axis label
+plt.ylabel("Overall Ratings") # Y axis label
+plt.tight_layout() # Make the chart tighter to fit all the information properly
 plt.show()
-
-print(sample_dataGuards)
